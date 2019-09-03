99942eb4d18ba08f163844ddb71af270c1741cec
Ilja Tkachuk <ilja@heathmont.net>
2019-09-03 11:25:20 +0300

    :design_values compile-time Application env

--- lib/bennu/design.ex
+++ lib/bennu/design.ex
@@ -3,8 +3,9 @@ defmodule Bennu.Design do
 
   defenum(
     database_type: :bennu_design,
-    values: [
-      :DEFAULT_COREUI
-    ]
+    values:
+      Application.get_env(:bennu, :design_values, [])
+      |> Enum.concat([:DEFAULT_COREUI])
+      |> Enum.uniq()
   )
 end
