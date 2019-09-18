eba40e9da55254b459dbd250a8d0be702469019e
Ilja Tkachuk <ilja@heathmont.net>
2019-09-18 16:04:53 +0300

    parent_pid -> root_pid for more efficient PropagateRedirect

--- lib/bennu/propagate_redirect.ex
+++ lib/bennu/propagate_redirect.ex
@@ -1,22 +1,32 @@
 defmodule Bennu.PropagateRedirect do
   defmacro __using__(_) do
     quote location: :keep do
-      require Phoenix.LiveView.Socket, as: Socket
-
-      defp propagate_redirect(%Socket{parent_pid: nil} = socket, params) do
+      defp propagate_redirect(
+             %Phoenix.LiveView.Socket{root_pid: nil} = socket,
+             params
+           ) do
         {:stop, Phoenix.LiveView.redirect(socket, params)}
       end
 
-      defp propagate_redirect(%Socket{parent_pid: pid} = socket, params) do
+      defp propagate_redirect(
+             %Phoenix.LiveView.Socket{root_pid: pid} = socket,
+             params
+           ) do
         _ = send(pid, {:propagate_redirect, params})
         socket
       end
 
-      def handle_info({:propagate_redirect, params}, %Socket{parent_pid: nil} = socket) do
+      def handle_info(
+            {:propagate_redirect, params},
+            %Phoenix.LiveView.Socket{root_pid: nil} = socket
+          ) do
         {:stop, Phoenix.LiveView.redirect(socket, params)}
       end
 
-      def handle_info({:propagate_redirect, _} = msg, %Socket{parent_pid: pid} = socket) do
+      def handle_info(
+            {:propagate_redirect, _} = msg,
+            %Phoenix.LiveView.Socket{root_pid: pid} = socket
+          ) do
         _ = send(pid, msg)
         {:noreply, socket}
       end
