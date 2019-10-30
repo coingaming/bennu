1445446cbc5aa3db9d27c740d9c2e88e5a53e048
Reio Piller <reio@heathmont.net>
2019-10-30 23:59:30 +0200

    default scalars

--- lib/bennu/component/column/schema.ex
+++ lib/bennu/component/column/schema.ex
@@ -2,6 +2,11 @@ use Bennu.Component
 
 defcomponent Component.Column do
   input do
+    width(
+      min_qty: 1,
+      max_qty: 1,
+      type: Integer
+    )
     rows(
       min_qty: nil,
       max_qty: nil,
