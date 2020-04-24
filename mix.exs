b114933b042dd882f9553698bf6d2adcdc4646da
Reio Piller <reio@heathmont.net>
2020-04-24 16:43:03 +0300

    upgraded phoenix to 1.5

--- mix.exs
+++ mix.exs
@@ -31,15 +31,15 @@ defmodule Bennu.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:phoenix, "~> 1.4.10"},
-      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.5"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
       {:ecto_sql, "~> 3.2"},
       {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
+      {:jason, "~> 1.2"},
+      {:plug_cowboy, "~> 2.2"},
       {:gen_enum, "~> 0.4", organization: "coingaming"},
       {:typable, "~> 0.3"},
       {:defnamed, "~> 0.1.3"},
