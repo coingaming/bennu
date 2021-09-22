1491399d4c04bdf06102faa1692da7b59e53ebb3
Tankut Senturk <tankut.senturk@heathmont.net>
2021-09-22 16:10:32 +0300

    Add optional assigns return value to evaluate callback

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Bennu.MixProject do
   def project do
     [
       app: :bennu,
-      version: "0.1.0",
-      elixir: "~> 1.9",
+      version: "0.2.0",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
